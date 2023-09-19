f10917101c033ca1ac1b4a2e5a8afb2aea2281f4
Laboratorio de Telecomunicaciones <cisco@academico.unab.ad>
2023-09-19 18:33:35 -0500

    Actualizacion nombre secciones

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portal Web</title>
+</head>
+<body>
+    <h1>Portal web Parcial</h1>
+    
+    <form action="seccion_1.php">
+        <button type="submit">
+        <h2>Seccion 1</h2>
+    </button>
+    <input type="hidden" value="Seccion 1" />
+    
+    <form action="seccion_2.php">
+        <button type="submit">
+        <h2>Seccion 2</h2>
+    </button>
+    <input type="hidden" value="Seccion 2" />
+   
+    <form action="seccion_5.php">
+        <button type="submit">
+        <h2>Seccion 5</h2>
+    </button>
+    <input type="hidden" value="Seccion 5" />
+   
+    <form action="seccion_6.php">
+        <button type="submit">
+        <h2>Seccion 6</h2>
+    </button>
+    <input type="hidden" value="Seccion 6" />
+
+</form>
+</body>
+</html>
+
+
+
---
+++ seccion_5.php
@@ -0,0 +1,21 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Seccion 5</title>
+</head>
+<body>
+    <h1>Seccion 5</h1>
+    <form action="index.php">
+        <button type="submit">
+        <h2>Volver</h2>
+    </button>
+    <input type="hidden" value="Volver" />
+</form>
+</body>
+</html>
