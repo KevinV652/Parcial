f4d6cd1a8c15665d66a0b141bf6df62aedd8182a
ArleyParra <116411921+ArleyParra@users.noreply.github.com>
2023-09-19 19:41:26 -0500

    Update seccion_5.php

--- seccion_5.php
+++ seccion_5.php
@@ -23,27 +23,4 @@
 </form>
 </body>
 
-<?php
-
-function esCubifinito($numero) {
-    $cubifinitos = array(1, 153, 370, 371, 407);
-    return in_array($numero, $cubifinitos);
-}
-
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["analizar"])) {
-    $numeroIngresado = $_POST["numero"];
-    
-    if (is_numeric($numeroIngresado)) {
-        if (esCubifinito($numeroIngresado)) {
-            echo "<p>$numeroIngresado es un número cubifinito.</p>";
-        } else {
-            echo "<p>$numeroIngresado no es un número cubifinito.</p>";
-        }
-    } else {
-        echo "<p>Por favor, ingrese un número válido.</p>";
-    }
-}
-?>
-
-
 </html>
