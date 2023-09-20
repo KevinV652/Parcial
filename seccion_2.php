be75de21e01e7aafbdeb1e0a5e80669d62df5600
Yulder Andres Aponte Carrascal <you@example.com>
2023-09-19 22:24:32 -0500

    ultima modificacion

--- seccion_2.php
+++ seccion_2.php
@@ -1,56 +1,76 @@
 <?php
-class Fiesta {
-    private $totalPersonas = 0;
-    private $sumaEdades = 0;
-    private $edadMasJoven = PHP_INT_MAX;
+class CalculadoraEdades {
+    private $edades;
 
-    public function agregarPersona($edad) {
-        if ($edad == 0) {
-            return; // Salir del método si se ingresa 0
-        }
-
-        if ($edad < 18) {
-            echo "Lo siento, no se permite el ingreso de menores de edad." . PHP_EOL;
-            return; // Ignorar edades menores de 18
-        }
-
-        $this->totalPersonas++;
-        $this->sumaEdades += $edad;
-
-        if ($edad < $this->edadMasJoven) {
-            $this->edadMasJoven = $edad;
-        }
+    public function __construct($edades) {
+        $this->edades = $edades;
     }
 
-    public function calcularPromedioEdades() {
-        if ($this->totalPersonas > 0) {
-            return $this->sumaEdades / $this->totalPersonas;
+    public function calcularPromedio() {
+        $edadesArray = explode(',', $this->edades);
+        $edadesValidas = $this->filtrarEdadesValidas($edadesArray);
+
+        $totalEdades = count($edadesValidas);
+
+        if ($totalEdades > 0) {
+            $sumaEdades = array_sum($edadesValidas);
+            return $sumaEdades / $totalEdades;
         } else {
             return 0;
         }
     }
 
-    public function obtenerEdadMasJoven() {
-        return $this->edadMasJoven;
+    public function encontrarEdadMasBaja() {
+        $edadesArray = explode(',', $this->edades);
+        $edadesValidas = $this->filtrarEdadesValidas($edadesArray);
+
+        if (!empty($edadesValidas)) {
+            return min($edadesValidas);
+        } else {
+            return 0;
+        }
     }
 
-    public function obtenerTotalPersonas() {
-        return $this->totalPersonas;
+    private function filtrarEdadesValidas($edadesArray) {
+        return array_filter($edadesArray, function ($edad) {
+            return is_numeric($edad) && $edad >= 18;
+        });
+    }
+
+    public function contarPersonasValidas() {
+        $edadesArray = explode(',', $this->edades);
+        $edadesValidas = $this->filtrarEdadesValidas($edadesArray);
+        return count($edadesValidas);
     }
 }
 
-// Crear una instancia de la clase Fiesta
-$fiesta = new Fiesta();
-
-// Solicitar edades y realizar cálculos hasta que se ingrese 0
-while (true) {
-    $edad = intval(readline("Ingrese la edad (o 0 para finalizar): "));
-    $fiesta->agregarPersona($edad);
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $edades = isset($_POST['Edades']) ? $_POST['Edades'] : '';
+    $calculadora = new CalculadoraEdades($edades);
 }
-
-// Mostrar resultados
-echo "Resultados:" . PHP_EOL;
-echo "Total de personas que asistieron a la fiesta: " . $fiesta->obtenerTotalPersonas() . PHP_EOL;
-echo "Promedio de edades: " . $fiesta->calcularPromedioEdades() . PHP_EOL;
-echo "Edad de la persona más joven que asistió: " . $fiesta->obtenerEdadMasJoven() . PHP_EOL;
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Calculadora de Edades (POO)</title>
+</head>
+<body>
+    <h1>Ingrese las edades mayores de 18 años y haga clic en "Calcular" :</h1>
+    <h6> (si llegas a ingresar edad menores a 18 solo se mostrara en edades ingresadas para mostrar que valor esta de mas pero esos valores menores a 18 no se van a tener en cuenta para el calculo)</h6>
+    <form method="post" action="">
+        <label for="Edades">Edades (separadas por comas):</label>
+        <input type="text" id="Edades" name="Edades" required>
+        <br>
+        <input type="submit" value="Calcular">
+    </form>
+
+    <?php if (isset($calculadora)): ?>
+        <h2>Resultados:</h2>
+        <p>Edades ingresadas: <?php echo $edades; ?></p>
+        <p>Total de personas: <?php echo $calculadora->contarPersonasValidas(); ?></p>
+        <p>Promedio de las edades: <?php echo $calculadora->calcularPromedio(); ?></p>
+        <p>Edad más baja: <?php echo $calculadora->encontrarEdadMasBaja(); ?></p>
+    <?php endif; ?>
+</body>
+</html>
